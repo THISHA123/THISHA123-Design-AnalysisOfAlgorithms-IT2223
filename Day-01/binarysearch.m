a1ba596b08f923918554e2d343368a9b8604ed35
T.Thishanthani <thishanthani09@gmail.com>
2025-05-26 22:57:40 +0530

    Add files via upload

---
+++ Day-01/binarysearch.m
@@ -0,0 +1,19 @@
+arr=[3,6,8,12,14,25,29,31,36,42,47,53,55,62];
+target = 42;
+
+low = 1;
+high = length(arr);
+
+while low <= high
+mid= floor ((low+high) / 2 );
+
+if arr(mid)== target
+    index=mid;
+    break;
+elseif arr(mid) < target
+    low = mid + 1;
+
+else
+    high= mid - 1;
+end
+end
