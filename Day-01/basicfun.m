2c18bde611916e0dc80a732255b5605b840f8b65
T.Thishanthani <thishanthani09@gmail.com>
2025-05-26 22:48:41 +0530

    Delete Day-01/basicfun.m

--- Day-01/basicfun.m
+++
@@ -1,9 +0,0 @@
-% This script demonstrates a basic if-else conditional statement in MATLAB
-
-num = 4;  % Initialize variable
-
-if num > 0
-    disp('Positive')  % Display if condition is true
-else
-    disp('Negative')  % Display if condition is false
-end
